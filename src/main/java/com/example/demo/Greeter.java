2674f590870ad03fee8d9914040a7bfdbef52398
Pat Turner <patrick.turner@nhs.net>
2020-09-29 11:52:04 +0100

    iter 4 good morning

--- src/main/java/com/example/demo/Greeter.java
+++ src/main/java/com/example/demo/Greeter.java
@@ -1,6 +1,27 @@
 package com.example.demo;
 
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Greeter {
+
+	private static final LocalDateTime MORNING_BEGINS = localDateTime(6, 0);
+	private static final LocalDateTime AFTERNOON_BEGINS = localDateTime(12, 0);
+	
+	private static LocalDateTime localDateTime(int hours, int minutes) {
+		return LocalDateTime.of(LocalDate.now(), LocalTime.of(hours, minutes));
+	}
+
+	private Clock clock;
+	
+	public Greeter() {
+		this(Clock.systemDefaultZone());
+	}
+	public Greeter(Clock clock) {
+		this.clock = clock;
+	}
 
 	public Object greet(String name) {
 
@@ -11,10 +32,20 @@ public class Greeter {
 			throw new IllegalArgumentException("'name' cannot be empty");
 		}
 		
-		String greeting = name.trim();
-		greeting = greeting.substring(0, 1).toUpperCase() + greeting.substring(1);
-				
-		return "Hello " + greeting;
+		String properName = name.trim();
+		properName = properName.substring(0, 1).toUpperCase() + properName.substring(1);
+		
+		return greeting() + properName;
+	}
+	
+	private String greeting() {
+		if (LocalDateTime.now(clock).isBefore(MORNING_BEGINS)) {
+			return "Hello ";
+		}
+		if (LocalDateTime.now(clock).isBefore(AFTERNOON_BEGINS)) {
+			return "Good morning ";
+		}
+		return "Hello ";
 	}
 
 }
--- src/test/java/com/example/demo/GreeterTest.java
+++ src/test/java/com/example/demo/GreeterTest.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
 import org.junit.jupiter.api.Test;
 
 class GreeterTest {
@@ -25,19 +31,37 @@ class GreeterTest {
 	@Test
 	void shouldGreetHelloName() {
 
-		assertEquals("Hello Bob", new Greeter().greet("Bob"));
+		assertEquals("Hello Bob", greeterAtTimeOfDay(12, 0).greet("Bob"));
 	}
 	
 	@Test
 	void shouldTrimInput() throws Exception {
 		
-		assertEquals("Hello Bob", new Greeter().greet(" Bob "));
+		assertEquals("Hello Bob", greeterAtTimeOfDay(12, 0).greet(" Bob "));
 	}
 
 	@Test
 	void shouldCapitaliseFirstLetter() throws Exception {
 		
-		assertEquals("Hello Bob", new Greeter().greet("bob"));
+		assertEquals("Hello Bob", greeterAtTimeOfDay(12, 0).greet("bob"));
+	}
+	
+	@Test
+	void shouldGreetGoodMorning() throws Exception {
+		
+		assertEquals("Good morning Bob", greeterAtTimeOfDay(6, 0).greet("bob"));
+	}
+
+	private Greeter greeterAtTimeOfDay(int hours, int minutes) {
+		
+		Clock clock = clockAtTimeOfDay(hours, minutes);
+		return new Greeter(clock);
+	}
+
+	private Clock clockAtTimeOfDay(int hours, int minutes) {
+		return Clock.fixed(
+				LocalDateTime.of(LocalDate.now(), LocalTime.of(hours, minutes)).atZone(ZoneId.systemDefault()).toInstant(), 
+				ZoneId.systemDefault());
 	}
 
 }
