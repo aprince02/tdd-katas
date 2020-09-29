36978bf148d6f5e6ead0cb3f9b6e6dcf1880b691
Pat Turner <patrick.turner@nhs.net>
2020-09-29 11:21:36 +0100

    iter 3 capitalise

--- src/main/java/com/example/demo/Greeter.java
+++ src/main/java/com/example/demo/Greeter.java
@@ -4,7 +4,17 @@ public class Greeter {
 
 	public Object greet(String name) {
 
-		return "Hello " + name;
+		if (name == null) {
+			throw new IllegalArgumentException("'name' cannot be null");
+		}
+		if (name.isBlank()) {
+			throw new IllegalArgumentException("'name' cannot be empty");
+		}
+		
+		String greeting = name.trim();
+		greeting = greeting.substring(0, 1).toUpperCase() + greeting.substring(1);
+				
+		return "Hello " + greeting;
 	}
 
 }
--- src/test/java/com/example/demo/GreeterTest.java
+++ src/test/java/com/example/demo/GreeterTest.java
@@ -1,15 +1,43 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 class GreeterTest {
 
 	@Test
+	void shouldThrowExceptionOnNull() {
+		assertThrows(IllegalArgumentException.class, () -> new Greeter().greet(null));
+	}
+	
+	@Test
+	void shouldThrowExceptionOnEmpty() {
+		assertThrows(IllegalArgumentException.class, () -> new Greeter().greet(""));
+	}
+	
+	@Test
+	void shouldThrowExceptionOnBlank() {
+		assertThrows(IllegalArgumentException.class, () -> new Greeter().greet(" "));
+	}
+	
+	@Test
 	void shouldGreetHelloName() {
 
 		assertEquals("Hello Bob", new Greeter().greet("Bob"));
+	}
+	
+	@Test
+	void shouldTrimInput() throws Exception {
+		
+		assertEquals("Hello Bob", new Greeter().greet(" Bob "));
+	}
+
+	@Test
+	void shouldCapitaliseFirstLetter() throws Exception {
+		
+		assertEquals("Hello Bob", new Greeter().greet("bob"));
 	}
 
 }
